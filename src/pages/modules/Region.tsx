b0d65764a7f8246c2893f97fd13a102c0b8875f3
Minyoung Choi <youngdev57@gmail.com>
2025-08-10 12:38:32 +0900

    feat: 누락 페이지 callout 추가

--- src/pages/modules/Region.tsx
+++ src/pages/modules/Region.tsx
@@ -1,10 +1,11 @@
+import Callout from "@/components/common/Callout";
 import PageHeader from "@/components/common/Information";
 
 export default function Region() {
   return (
     <>
       <PageHeader title="Region" parentPath="Modules" />
-      <Callout description="목록 페이지네이션 및 검색 옵션 자동화를 위한 모듈입니다." />
+      <Callout description="지역 정보 데이터를 가공하는 모듈입니다." />
     </>
   );
 }
