414b2e509a0924d567cd198e57857e9c373e1cb0
Minyoung Choi <youngdev57@gmail.com>
2025-08-10 12:28:15 +0900

    feat: modules 하위 페이지 추가 및 라우팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "boxicons/css/boxicons.min.css";
-import "./styles/reset.css";
-import "./styles/global.css";
+import "@/styles/reset.css";
+import "@/styles/global.css";
 import Dashboard from "@/components/Dashboard";
 import MainPage from "@/pages/MainPage";
 import Api from "@/pages/modules/Api";
 import Pager from "@/pages/modules/Pager";
 import FileManager from "@/pages/modules/FileManager";
+import ThumbnailManager from "@/pages/modules/ThumbnailManager";
+import Region from "@/pages/modules/Region";
 import Form from "@/pages/common/Form";
 import Grid from "@/pages/common/Grid";
 import Ui from "@/pages/common/Ui";
@@ -23,6 +25,11 @@ function App() {
           <Route path="modules/api" element={<Api />} />
           <Route path="modules/pager" element={<Pager />} />
           <Route path="modules/file-manager" element={<FileManager />} />
+          <Route
+            path="modules/thumbnail-manager"
+            element={<ThumbnailManager />}
+          />
+          <Route path="modules/region-manager" element={<Region />} />
           <Route path="common/form" element={<Form />} />
           <Route path="common/grid" element={<Grid />} />
           <Route path="common/ui" element={<Ui />} />
---
+++ src/pages/modules/Region.tsx
@@ -0,0 +1,5 @@
+import PageHeader from "@/components/common/Information";
+
+export default function Region() {
+  return <PageHeader title="Region" parentPath="Modules" />;
+}
